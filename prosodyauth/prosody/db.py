228083d2276c820182802880c912d17eea165537
Travis Veazey <travisvz@gmail.com>
2015-10-15 14:12:54 -0800

    Rework db module to remove unnecessary object

--- prosodyauth/prosody/db.py
+++ prosodyauth/prosody/db.py
@@ -1,42 +1,37 @@
 from django.db import connection
 
 
-class ProsodyDatastore:
-    @staticmethod
-    def get_data_store(username, domain, store):
-        data = {}
-        with connection.cursor() as cursor:
-            cursor.execute(
-                    'SELECT key, value FROM prosody.prosody WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s',
-                    [username, domain, store]
-                    )
+def get_data_store(username, domain, store):
+    data = {}
+    with connection.cursor() as cursor:
+        cursor.execute(
+                'SELECT key, value FROM prosody.prosody WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s',
+                [username, domain, store]
+                )
 
-            data = dict(cursor.fetchall())
+        data = dict(cursor.fetchall())
 
-        return data
+    return data
 
-    @staticmethod
-    def get_value(username, domain, store, key):
-        try:
-            data = ProsodyDatastore.get_data_store(username, domain, store)[key]
-        except KeyError:
-            data = None
+def select(username, domain, store, key):
+    try:
+        data = get_data_store(username, domain, store)[key]
+    except KeyError:
+        data = None
 
-        return data
+    return data
 
-    @staticmethod
-    def set_value(username, domain, store, key, value):
-        with connection.cursor() as cursor:
-            cursor.execute(
-                    'UPDATE prosody.prosody SET value=%s WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s AND key=%s',
-                    [value, username, domain, store, key]
-                    )
+def update(username, domain, store, key, value):
+    with connection.cursor() as cursor:
+        cursor.execute(
+                'UPDATE prosody.prosody SET value=%s WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s AND key=%s',
+                [value, username, domain, store, key]
+                )
 
-    @staticmethod
-    def insert_value(username, domain, store, key, value):
-        with connection.cursor() as cursor:
-            cursor.execute(
-                    'INSERT INTO prosody.prosody SET value=%s, UPPER("user")=UPPER(%s), UPPER(host)=UPPER(%s), store=%s, key=%s',
-                    [value, username, domain, store, key]
-                    )
+def insert(username, domain, store, key, value):
+    with connection.cursor() as cursor:
+        cursor.execute(
+                'INSERT INTO prosody.prosody SET value=%s, UPPER("user")=UPPER(%s), UPPER(host)=UPPER(%s), store=%s, key=%s',
+                [value, username, domain, store, key]
+                )
 
