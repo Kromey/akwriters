165fe54c9c546ff7c28487f684baa4d1dad2d72f
Travis Veazey <travisvz@gmail.com>
2015-10-16 15:35:58 -0800

    Refactoring password authentication to separate module

---
+++ prosodyauth/authenticate.py
@@ -0,0 +1,33 @@
+from passlib.hash import scram
+import hmac
+from hashlib import sha1
+
+
+def verify_password(password, salt, iterations, stored_key, server_key):
+    salted_pass = salt_password(password, salt, iterations)
+
+    return stored_key == compute_stored_key(salted_pass) and server_key == compute_server_key(salted_pass)
+
+
+def make_salt():
+    """
+    Generate a random salt
+
+    A hex-encoded random UUID has plenty of entropy to be secure enough for our
+    needs.
+    """
+    return uuid.uuid4().hex
+
+
+def salt_password(password, salt, iterations):
+    return scram.derive_digest(password, salt.encode('utf-8'), int(iterations), 'sha-1')
+
+
+def compute_stored_key(salted_pass):
+    h = sha1()
+    h.update(hmac.new(salted_pass, b'Client Key', sha1).digest())
+    return h.hexdigest()
+
+
+def compute_server_key(salted_pass):
+    return hmac.new(salted_pass, b'Server Key', sha1).hexdigest()
