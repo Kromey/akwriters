88aa4c6a6ffd0d8c06305ca6d43df925eaf4f10e
Travis Veazey <travisvz@gmail.com>
2015-10-16 16:22:41 -0800

    Long-standing bug from removing the prosody schema

--- prosodyauth/prosody/db.py
+++ prosodyauth/prosody/db.py
@@ -5,7 +5,7 @@ def get_data_store(username, domain, store):
     data = {}
     with connection.cursor() as cursor:
         cursor.execute(
-                'SELECT key, value FROM prosody.prosody WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s',
+                'SELECT key, value FROM prosody WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s',
                 [username, domain, store]
                 )
 
@@ -24,14 +24,14 @@ def select(username, domain, store, key):
 def update(username, domain, store, key, value):
     with connection.cursor() as cursor:
         cursor.execute(
-                'UPDATE prosody.prosody SET value=%s WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s AND key=%s',
+                'UPDATE prosody SET value=%s WHERE UPPER("user")=UPPER(%s) AND UPPER(host)=UPPER(%s) AND store=%s AND key=%s',
                 [value, username, domain, store, key]
                 )
 
 def insert(username, domain, store, key, value):
     with connection.cursor() as cursor:
         cursor.execute(
-                'INSERT INTO prosody.prosody (host, "user", store, key, value) VALUES (%s, %s, %s, %s, %s)',
+                'INSERT INTO prosody (host, "user", store, key, value) VALUES (%s, %s, %s, %s, %s)',
                 [domain, username, store, key, value]
                 )
 
