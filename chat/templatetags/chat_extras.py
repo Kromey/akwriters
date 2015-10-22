95200cf3ffbedca3f897d5e51fecf026f8d47c7b
Travis Veazey <travisvz@gmail.com>
2015-10-22 14:43:45 -0800

    Add new prosody_proto template tags
    Now have a simple template tag that lets me standardize appearances of the "XMPP" name and logo without having to copy/paste the raw HTML. Yay DRY!

--- chat/templatetags/chat_extras.py
+++ chat/templatetags/chat_extras.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 register = template.Library()
 
@@ -7,3 +8,22 @@ register = template.Library()
 @register.simple_tag
 def prosody_domain():
     return settings.PROSODY_DEFAULT_DOMAIN
+
+@register.simple_tag
+def prosody_proto():
+    # Shift it upward a bit so it sits on the same line as the text
+    style = 'margin-top: -4px; margin-right: 2px;'
+
+    # Set height and width for the image
+    width = 16
+    height = 16
+
+    # The protocol itself
+    proto = 'XMPP'
+
+    # The staticfiles path to the protocol image/logo
+    img = static('img/xmpp.png')
+
+    # Now put it all together and return the result
+    return '<img src="{}" alt="" style="{}" width="{}" height="{}" />{}'.format(
+            img, style, width, height, proto)
