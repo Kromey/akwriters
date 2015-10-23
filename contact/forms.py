e04d3bfd20879d0e8e404a3fff4ab37b914cd303
Travis Veazey <travisvz@gmail.com>
2015-10-22 21:13:41 -0800

    Add ContactForm

---
+++ contact/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+from prosodyauth.forms import PlaceholderForm
+from simplecaptcha import captcha
+
+
+contact_reasons = (
+        ('question', 'Question'),
+        ('problem', 'Problem'),
+        ('suggestion', 'Suggestion'),
+        ('other', 'Other'),
+        )
+
+class ContactForm(PlaceholderForm):
+    username = forms.CharField(widget=forms.HiddenInput)
+    ip_address = forms.GenericIPAddressField(widget=forms.HiddenInput)
+    subject = forms.ChoiceField(choices=contact_reasons)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
