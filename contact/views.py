dd32884b65e17222db401f30719c62f0c9a29e7b
Travis Veazey <travisvz@gmail.com>
2015-10-22 21:50:48 -0800

    Initial view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import binascii
+
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.db import IntegrityError, transaction
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.http import Http404
+
+
+from contact.forms import ContactForm
 
 # Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success('Success!')
+        else:
+            messages.error('Boo! Hiss!')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact.html', {'form': form})
+
