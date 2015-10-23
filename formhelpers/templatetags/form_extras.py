3a33b7a81c196a13ef9878fe209ed0becd0dad21
Travis Veazey <travisvz@gmail.com>
2015-10-23 12:14:33 -0800

    Refactor fi_field tag to be more DRY

--- formhelpers/templatetags/form_extras.py
+++ formhelpers/templatetags/form_extras.py
@@ -5,6 +5,20 @@ register = template.Library()
 
 
 @register.simple_tag
-def fi_field(field, icon='_none'):
-    return render_to_string('formhelpers/fi_field.html', {'field': field, 'icon': icon})
+def fi_field(field, icon=None):
+    if icon:
+        label_width = 2
+    else:
+        label_width = 4
+
+    widget_width = 12 - label_width
+
+    context = {
+            'field': field,
+            'icon': icon,
+            'label_width': label_width,
+            'widget_width': widget_width,
+            }
+
+    return render_to_string('formhelpers/fi_field.html', context)
 
