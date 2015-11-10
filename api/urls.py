564075cbb66c6e79a6225d7f678aea804075b966
Travis Veazey <travisvz@gmail.com>
2015-11-09 15:52:24 -0900

    Switch from generic TemplateView to new StatusView

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
+
+from .views import StatusView
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fbxnano.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url('^status$', TemplateView.as_view(template_name='api/status.html'), name='status'),
+    url('^status$', StatusView.as_view(), name='status'),
 )
