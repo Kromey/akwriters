921d9eb61d893e544500191b43a816289493804d
Travis Veazey <travisvz@gmail.com>
2015-11-09 15:51:50 -0900

    First class-based API view, StatusView
    This API method/view simply returns a hardcoded "website up" response, relying on the fact that if it were not up the webserver would be returning some other response instead

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
+
+
+from .utils import ApiResponse
 
 # Create your views here.
+
+class StatusView(View):
+    def get(self, request):
+        status = {'status': 'The Website is UP! :)'}
+        return ApiResponse(status)
