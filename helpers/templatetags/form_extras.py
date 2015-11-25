4e57bd1617237b63e767ea61d4b4fd456b6e0031
Travis Veazey <travisvz@gmail.com>
2015-11-25 10:09:30 -0900

    Remove vestigial Foundation code

--- helpers/templatetags/form_extras.py
+++ helpers/templatetags/form_extras.py
@@ -5,24 +5,6 @@ register = template.Library()
 
 
 @register.simple_tag
-def fi_field(field, icon=None):
-    if icon:
-        label_width = 2
-    else:
-        label_width = 4
-
-    widget_width = 12 - label_width
-
-    context = {
-            'field': field,
-            'icon': icon,
-            'label_width': label_width,
-            'widget_width': widget_width,
-            }
-
-    return render_to_string('helpers/fi_field.html', context)
-
-@register.simple_tag
 def bs_field(field, icon=None):
     if icon:
         label_width = 2
