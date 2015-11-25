e6ddc5ebc4308cbd128ce04403efd86826afb29a
Travis Veazey <travisvz@gmail.com>
2015-11-24 16:57:51 -0900

    New bs_field template tag for Bootstrap-based fields
    It's not easy getting Django forms and Bootstrap to play nice together, as the latter requires a superfluous CSS class on all form fields, but the former doesn't make it easy to do that in front-end logic...

--- formhelpers/templatetags/form_extras.py
+++ formhelpers/templatetags/form_extras.py
@@ -22,3 +22,26 @@ def fi_field(field, icon=None):
 
     return render_to_string('formhelpers/fi_field.html', context)
 
+@register.simple_tag
+def bs_field(field, icon=None):
+    if icon:
+        label_width = 2
+    else:
+        label_width = 4
+
+    widget_width = 12 - label_width
+
+    # Have to add the form-control tag; why can't Bootstrap just assume this??
+    # Possibly worse, Django makes me manually compile it into the widget here!
+    widget = field.as_widget(attrs={"class":"form-control"})
+
+    context = {
+            'field': field,
+            'widget': widget,
+            'icon': icon,
+            'label_width': label_width,
+            'widget_width': widget_width,
+            }
+
+    return render_to_string('formhelpers/bs_field.html', context)
+
