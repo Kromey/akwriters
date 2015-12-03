873eacb772e342468f2563a825e1b0be9238dfff
Travis Veazey <travisvz@gmail.com>
2015-12-03 09:40:49 -0900

    Adding test messages
    Why is the debug one not showing up in the template?

--- alerts/views.py
+++ alerts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 
 def test(request):
+    messages.debug(request, 'This is a debug alert')
+    messages.info(request, 'This is an info alert')
+    messages.success(request, 'This is a success alert')
+    messages.warning(request, 'This is a warning alert')
+    messages.error(request, 'This is an error alert')
+
     return render(request, 'alerts/test.html')
