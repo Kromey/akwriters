23af11b5e05b6f074b2c93962dad1f220cc77163
Travis Veazey <travisvz@gmail.com>
2015-12-07 16:57:26 -0900

    Fix form validation logic, add POST method to view
    Not all hooked up just yet, no passwords are changed and does not validate old password yet.

--- account/forms.py
+++ account/forms.py
@@ -12,6 +12,7 @@ class PasswordChangeForm(PlaceholderForm):
     old_password = fields.PassField(min_length=8, label='current password')
     new_password = fields.PassField(min_length=8, label='new password')
     new_password_confirm = fields.PassField(label='confirm password')
+    username = forms.CharField(widget=forms.HiddenInput, required=False)
 
     def render_password(self, render_value=True):
         for field_name in self.fields:
@@ -22,11 +23,11 @@ class PasswordChangeForm(PlaceholderForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        confirm = cleaned_data.get('password_confirm')
+        password = cleaned_data.get('new_password')
+        confirm = cleaned_data.get('new_password_confirm')
 
         if password and password != confirm:
-            self.add_error('password_confirm', ValidationError('Confirmation must match password', code='invalid'))
+            self.add_error('new_password_confirm', ValidationError('Confirmation must match password', code='invalid'))
         elif password and not authenticate.password_is_compliant(password, cleaned_data.get('username')):
-            self.add_error('password', ValidationError('Password does not meet requirements', code='invalid'))
+            self.add_error('new_password', ValidationError('Password does not meet requirements', code='invalid'))
 
--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.utils.decorators import method_decorator
+from django.contrib import messages
 
 
 from account.forms import PasswordChangeForm
@@ -10,6 +11,23 @@ from prosodyauth.mixins import LoginRequiredMixin
 
 class AccountSettingsView(LoginRequiredMixin, View):
     def get(self, request):
+        return self._render_settings(request)
+
+    def post(self, request):
+        # Ensure the logged-in user's username is here; needed for validation
+        post_data = request.POST.copy()
+        post_data['username'] = request.user.username
+        form = PasswordChangeForm(post_data)
+
+        messages.debug(request, post_data)
+        if form.is_valid():
+            messages.success(request, 'You have correctly entered a password')
+        else:
+            messages.error(request, 'You have incorrectly entered a password')
+
+        return self._render_settings(request)
+
+    def _render_settings(self, request):
         form = PasswordChangeForm()
 
         return render(request, 'account/settings.html', {'passform': form})
