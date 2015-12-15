6ebf8f4b25ac91cd3e0287e9ebf78f548970ddd0
Travis Veazey <travisvz@gmail.com>
2015-12-15 10:23:43 -0900

    Extend bs_field to support additional icon types
    Currently that means just the Glyphicons (come with Bootstrap) and Octicons, but could easily be extended to additional icon types as well.

--- helpers/templatetags/form_extras.py
+++ helpers/templatetags/form_extras.py
@@ -1,15 +1,25 @@
 from django import template
 from django.template.loader import render_to_string
 
+from helpers.templatetags.frontend_extras import glyphicon, octicon
+
 register = template.Library()
 
 
 @register.simple_tag
-def bs_field(field, icon=None):
+def bs_field(field, icon=None, icon_type='glyphicon'):
     if icon:
+        if icon_type == 'glyphicon':
+            icon_html = glyphicon(icon)
+        elif icon_type == 'octicon':
+            icon_html = octicon(icon)
+        else:
+            raise ValueError('Unknown icon_type: {}'.format(icon_type))
+
         label_width = 2
     else:
         label_width = 4
+        icon_html = None
 
     widget_width = 12 - label_width
 
@@ -20,7 +30,7 @@ def bs_field(field, icon=None):
     context = {
             'field': field,
             'widget': widget,
-            'icon': icon,
+            'icon': icon_html,
             'label_width': label_width,
             'widget_width': widget_width,
             }
