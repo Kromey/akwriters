69e868f04f206f2b3d5e95ac37982d8ccbd250df
Travis Veazey <travisvz@gmail.com>
2016-09-23 13:41:30 -0800

    Simplify User, add AnonymousUser

--- passwordless/models.py
+++ passwordless/models.py
@@ -6,16 +6,48 @@ class User(models.Model):
     """
     User model
 
-    This User model relies in part on the Prosody model for additional data
-    about the user, including password and the XMPP domain.
+    This User model eschews passwords, relying instead on emailed OTP tokens.
     """
 
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(null=True)
-    first_name = models.CharField(max_length=30, null=True)
-    last_name = models.CharField(max_length=30, null=True)
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    timezone = models.CharField(max_length=30, null=True)
+
+    @property
+    def is_authenticated(self):
+        #Used to tell authenticated Users from anonymous ones
+        return True
+
+    @property
+    def is_anonymous(self):
+        #This is not an anonymous user
+        return False
+
+    def __str__(self):
+        return self.username
+
+
+class AnonymousUser:
+    """
+    An object to represent an anonymous/unauthenticated user
+    """
+    username = ''
+    email = None
+    is_active = False
+    is_superuser = False
+    date_joined = None
+
+    @property
+    def is_authenticated(self):
+        #Anonymous sessions are not authenticated
+        return False
+
+    @property
+    def is_anonymous(self):
+        return True
+
+    def __str__(self):
+        return "Anonymous User"
 
