da5958a396c06ad66b36c26cf4540d0f6c5c62e5
Travis Veazey <travisvz@gmail.com>
2016-09-26 14:21:55 -0800

    New mixin for placeholder forms
    Using a mixin rather than an intermediary sub-class allows for more flexibility in the final form class, such as picking and choosing multiple mixins

--- helpers/forms.py
+++ helpers/forms.py
@@ -21,3 +21,25 @@ class PlaceholderForm(forms.Form):
                             { 'placeholder': field.widget.attrs.get('placeholder', field.label or field_name) }
                             )
 
+
+class PlaceholderFormMixin(object):
+    """
+    A form mixin for automatically adding placeholder text.
+
+    Forms that use this mixin will by default have all text, password, and
+    date input widgets display placeholder text equal to their label. This can
+    be overridden per form field by simply specifying a different placeholder
+    attribute.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name in self.fields:
+            field = self.fields.get(field_name)
+            if field:
+                if type(field.widget) in (forms.TextInput, forms.PasswordInput, forms.EmailInput, forms.DateInput):
+                    field.widget.attrs.update(
+                            { 'placeholder': field.widget.attrs.get('placeholder', field.label or field_name) }
+                            )
+
+
