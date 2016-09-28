92e2ff164cca2aba06a92c1f76711c9a6ba61c29
Travis Veazey <travisvz@gmail.com>
2016-09-27 16:59:36 -0800

    Add form validation methods

--- passwordless/forms.py
+++ passwordless/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 
+from passwordless.models import User
 from simplecaptcha import captcha
 
 
@@ -11,4 +12,20 @@ from helpers.forms import PlaceholderFormMixin
 class RegistrationForm(PlaceholderFormMixin, forms.Form):
     username = forms.CharField(min_length=3, max_length=30)
     email = forms.EmailField()
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if User.objects.filter(username__iexact=username).count() > 0:
+            raise ValidationError('That username is already taken', code='invalid')
+
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email').lower()
+
+        if User.objects.filter(email=email).count() > 0:
+            raise ValidationError('That email is already in use', code='invalid')
+
+        return email
 
