b0c63952a8210765045fc6fa4b775184966d6ae5
Travis Veazey <travisvz@gmail.com>
2016-09-29 09:46:30 -0800

    LoginForm now validates user

--- passwordless/forms.py
+++ passwordless/forms.py
@@ -12,6 +12,17 @@ from helpers.forms import PlaceholderFormMixin
 class LoginForm(PlaceholderFormMixin, forms.Form):
     username = forms.CharField(min_length=3, max_length=30, label="username or email")
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        try:
+            User.objects.get(username__iexact=username)
+        except User.DoesNotExist:
+            try:
+                User.objects.get(email__iexact=username)
+            except User.DoesNotExist:
+                raise forms.ValidationError('User could not be found')
+
 
 class RegistrationForm(LoginForm):
     email = forms.EmailField()
@@ -27,7 +38,7 @@ class RegistrationForm(LoginForm):
         username = self.cleaned_data.get('username')
 
         if User.objects.filter(username__iexact=username).count() > 0:
-            raise ValidationError('That username is already taken', code='invalid')
+            raise forms.ValidationError('That username is already taken', code='invalid')
 
         return username
 
@@ -35,7 +46,7 @@ class RegistrationForm(LoginForm):
         email = self.cleaned_data.get('email').lower()
 
         if User.objects.filter(email=email).count() > 0:
-            raise ValidationError('That email is already in use', code='invalid')
+            raise forms.ValidationError('That email is already in use', code='invalid')
 
         return email
 
