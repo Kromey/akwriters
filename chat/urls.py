9919ac9e151b1891b0f2ad517f2f84f63a9cd6af
Travis Veazey <travisvz@gmail.com>
2016-10-07 16:50:46 -0800

    Major refactor of chat pages
    Still need to re-address content...

--- chat/urls.py
+++ chat/urls.py
@@ -11,8 +11,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url('^$', TemplateView.as_view(template_name='chat/index.html'), name='index'),
-    url(r'^pidgin$', TemplateView.as_view(template_name='chat/pidgin.html'), name='pidgin'),
-    url(r'^adium$', TemplateView.as_view(template_name='chat/adium.html'), name='adium'),
-    url(r'^annika$', TemplateView.as_view(template_name='chat/annika.html'), name='annika'),
     url(r'^candy$', login_required(views.CandyView.as_view()), name='candy'),
 ]
