1d9462605e65a7e6d7988980d70fffbf01e6b259
Travis Veazey <travisvz@gmail.com>
2016-10-10 10:21:52 -0800

    Remove inline styles
    Mostly straightfoward operation that now enables a much stronger Content-Security-Policy header, though required creating some custom JS functions to handle setting progress bar widths.

---
+++ site_assets/js/akwriters.js
@@ -0,0 +1,23 @@
+$(function() {
+	$('[data-popup-uri]').click(function()
+	{
+		newWin = window.open($(this).data('popup-uri'), 'Candy', 'width=800,height=500');
+		if(!newWin || newWin.closed || typeof newWin.closed=='undefined')
+		{
+			alert("Your browser has blocked the pop-up. Please allow pop-ups for this site.");
+		}
+		return false;
+	});
+
+	//Calculate the % width of all progress bars
+	$('div[role=progressbar]').width(function() {
+		var vmin = parseFloat($(this).attr('aria-valuemin'));
+		var vmax = parseFloat($(this).attr('aria-valuemax'));
+		var vnow = parseFloat($(this).attr('aria-valuenow'));
+
+		var width = (vnow - vmin)/(vmax - vmin);
+		width = width * 100;
+		return width+"%";
+	});
+});
+
