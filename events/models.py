2cd3e427796a7b5eae5de13faa05677a6ecfa96b
Travis Veazey <travisvz@gmail.com>
2016-10-14 15:13:42 -0800

    Adding Calendar model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Calendar(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    remote_id = models.CharField(max_length=60)
+    css_class = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
