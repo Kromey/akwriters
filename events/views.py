d3ed70b355dc8a85c2f72683432d03a6d4f12670
Travis Veazey <travisvz@gmail.com>
2016-10-14 16:32:05 -0800

    Generate current month's calendar
    No longer relying on a hard-coded list of day boxes, we have an actual calendar!

--- events/views.py
+++ events/views.py
@@ -1,7 +1,24 @@
+import datetime
+import calendar
+
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils import timezone
+
 
 # Create your views here.
 class EventsView(TemplateView):
     template_name = 'events/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        today = timezone.localtime(timezone.now())
+        cal = calendar.Calendar(6)
+
+        context['dates'] = cal.monthdatescalendar(2016,10)
+        context['today'] = today
+
+        return context
 
