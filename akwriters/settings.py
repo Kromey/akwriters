a2532b1f06eef9e08fcecba1bf93af81e85c6f08
Travis Veazey <travisvz@gmail.com>
2016-10-17 10:10:38 -0800

    Fetch events from Google Calendar

--- akwriters/settings.py
+++ akwriters/settings.py
@@ -134,6 +134,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+GCAL_API_KEY = site_settings.GCAL_API_KEY
+
 
 # Custom settings
 CONTACT_EMAIL = site_settings.CONTACT_EMAIL
--- events/views.py
+++ events/views.py
@@ -1,10 +1,13 @@
 import datetime
 import calendar
+import json
+from urllib import request,parse
 
 
+from django.conf import settings
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.utils import timezone
+from django.utils import dateparse,timezone
 
 
 # Create your views here.
@@ -15,9 +18,38 @@ class EventsView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         today = timezone.localtime(timezone.now())
-        cal = calendar.Calendar(6)
+        dates = calendar.Calendar(6).monthdatescalendar(today.year, today.month)
 
-        context['dates'] = cal.monthdatescalendar(2016,10)
+        time_min = dates[0][0].isoformat() + 'T00:00:00Z'
+        api_key = settings.GCAL_API_KEY
+        url = 'https://www.googleapis.com/calendar/v3/calendars/{calendar}/events?timeMin={time_min}&key={api_key}'.format(
+                calendar=parse.quote('de0dm8l2ah81c1lvuqeq987480@group.calendar.google.com'),
+                time_min=parse.quote(time_min),
+                api_key=parse.quote(api_key),
+                )
+
+        rawdata = request.urlopen(url).read()
+        gdata = json.loads(rawdata.decode('utf8'))
+
+        events = {}
+        for event in gdata['items']:
+            date = dateparse.parse_datetime(event['start']['dateTime']).date().isoformat()
+            try:
+                events[date].append(event)
+            except KeyError:
+                events[date] = [event,]
+
+        cal = []
+        for week in dates:
+            cal.append([])
+            for date in week:
+                isodate = date.isoformat()
+                cal[-1].append({
+                    'date': date,
+                    'events': events.get(isodate, [])
+                    })
+
+        context['calendar'] = cal
         context['today'] = today
 
         return context
