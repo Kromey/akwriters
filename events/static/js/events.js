136ac764d4d395a2855f4637e56311d339627d3f
Travis Veazey <travisvz@gmail.com>
2016-10-17 15:39:39 -0800

    Call .blur() after toggling events

--- events/static/js/events.js
+++ events/static/js/events.js
@@ -1,13 +1,14 @@
 $(function() {
 	$('.gcal-control input[type=checkbox]').change(function() {
-		target = $(this).val()
+		target = $(this).val();
 		if($(this).is(':checked')) {
-			$(this).parent().removeClass('gcal-control-disabled')
-			$('.event.'+target).slideDown()
+			$(this).parent().removeClass('gcal-control-disabled');
+			$('.event.'+target).slideDown();
 		} else {
-			$(this).parent().addClass('gcal-control-disabled')
-			$('.event.'+target).slideUp()
+			$(this).parent().addClass('gcal-control-disabled');
+			$('.event.'+target).slideUp();
 		}
+		$(this).blur();
 	});
 
 	// Fix FF being stupid
