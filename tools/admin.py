42d5553878673dcbc45e7146164c86f418312152
Travis Veazey <travisvz@gmail.com>
2016-10-29 18:46:13 -0800

    Add models to Admin

--- tools/admin.py
+++ tools/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+
+from .models import Character,CharacterNotes,CharacterNotesAnswer
+
+
 # Register your models here.
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('name','owner',)
+    readonly_fields = ('owner',)
+
+
+@admin.register(CharacterNotes)
+class CharacterNotesAdmin(admin.ModelAdmin):
+    list_display = ('question','is_active',)
+
+
+@admin.register(CharacterNotesAnswer)
+class CharacterNotesAnswerAdmin(admin.ModelAdmin):
+    list_display = ('character','question','answer',)
+    readonly_fields = ('character','question','answer',)
+
