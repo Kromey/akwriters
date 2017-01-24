6cfc38eaeaf5a762e31b3a2639ec122788256910
Travis Veazey <travisvz@gmail.com>
2017-01-23 15:34:17 -0900

    New method to insert replies into a Topic

--- forum/models.py
+++ forum/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db import models
+from django.db import models,transaction
 from django.template.defaultfilters import slugify
 
 
@@ -46,6 +46,26 @@ class Topic(models.Model):
     @property
     def post_count(self):
         return self.op.reply_count + 1
+
+    @transaction.atomic
+    def insert_post(self, post, reply_to):
+        # We need to lock the topic tree, to prevent race conditions
+        if Post.objects.select_for_update().filter(topic=self):
+            pass
+
+        # Make sure nobody else has updated this in the meantime
+        reply_to.refresh_from_db()
+        # Make room for our post
+        Post.objects.filter(topic=self).filter(left__gt = reply_to.left).update(left = models.F('left') + 2)
+        Post.objects.filter(topic=self).filter(right__gt = reply_to.left).update(right = models.F('right') + 2)
+
+        # Now fit ourselves right into place
+        post.left = reply_to.left + 1
+        post.right = post.left + 1
+        # Let's just make sure we have the topic set right
+        post.topic = self
+
+        post.save()
 
 
 class Post(models.Model):
