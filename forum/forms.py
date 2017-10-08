47088dd1ed69207e6e74af98c1f6a4124493ed0c
Travis Veazey <travisvz@gmail.com>
2017-10-08 11:37:57 -0800

    Use Octicons in Markdown editor

--- forum/forms.py
+++ forum/forms.py
@@ -14,6 +14,7 @@ class PostForm(ModelForm):
                     attrs={
                         'data-provide':'markdown',
                         'data-hidden-buttons':'cmdHeading',
+                        'data-iconlibrary':'octicons',
                         }),
                 }
 
