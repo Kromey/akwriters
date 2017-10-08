884852eeb2dec07dccefc26595f097ec9ae8532b
Travis Veazey <travisvz@gmail.com>
2017-10-08 11:26:34 -0800

    Add autofocus to subject field

--- forum/forms.py
+++ forum/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm,Textarea
+from django.forms import ModelForm,Textarea,TextInput
 
 
 from .models import Post
@@ -9,6 +9,7 @@ class PostForm(ModelForm):
         model = Post
         fields = ('subject','body')
         widgets = {
+                'subject': TextInput(attrs={'autofocus':'autofocus'}),
                 'body': Textarea(
                     attrs={
                         'data-provide':'markdown',
