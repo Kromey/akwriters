5c4b47d68219ad300cf3f7af1e6b4af88197cae0
Travis Veazey <travisvz@gmail.com>
2017-11-04 12:52:40 -0800

    Make URLs configurable
    Should be set in site_settings.py as keys under THE_BOT: "webhook" is the base URL to the bot; "base_url" is the base URL to the site so we can send absolute URLs in our notifications

--- forum/signals.py
+++ forum/signals.py
@@ -1,6 +1,7 @@
 import requests
 
 
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -8,23 +9,22 @@ from django.dispatch import receiver
 from .models import Post
 
 
-@receiver(post_save, sender=Post, dispatch_uid='notify_bot')
-def notify_bot(sender, instance, **kwargs):
-    with open('/tmp/signal.log', 'a') as fh:
-        fh.write('{pid} {subject} {op}\n'.format(
-            pid=instance.pk,
-            subject=instance.subject,
-            op=instance.is_op,
-            ))
-    if instance.is_op:
-        data = {
-                'user': instance.user.username,
-                'topic': instance.subject,
-                'board': instance.board.title,
-                'url': instance.get_absolute_url(),
-                }
-        resp = requests.post('http://localhost:3143/forum_post', json=data)
+BOT_URL = settings.THE_BOT.get('webhook', '').strip().rstrip('/')
+BASE_URL = settings.THE_BOT.get('base_url', '').strip().rstrip('/')
 
-        if not resp.ok:
-            raise Exception(resp.content)
+
+if BOT_URL and BASE_URL:
+    @receiver(post_save, sender=Post, dispatch_uid='notify_bot')
+    def notify_bot(sender, instance, **kwargs):
+        if instance.is_op:
+            data = {
+                    'user': instance.user.username,
+                    'topic': instance.subject,
+                    'board': instance.board.title,
+                    'url': BASE_URL + instance.get_absolute_url(),
+                    }
+            resp = requests.post(BOT_URL+'/forum_post', json=data)
+
+            if not resp.ok:
+                raise Exception(resp.content)
 
