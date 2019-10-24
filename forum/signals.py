f7b9cd2c982243a23551118d9e705acb52cfd07d
Travis Veazey <travisvz@gmail.com>
2019-10-24 12:05:09 -0800

    Disabling bot notification hook
    Currently no bot to notify anyway

--- forum/signals.py
+++ forum/signals.py
@@ -9,22 +9,22 @@ from django.dispatch import receiver
 from .models import Post
 
 
-BOT_URL = settings.THE_BOT.get('webhook', '').strip().rstrip('/')
-BASE_URL = settings.THE_BOT.get('base_url', '').strip().rstrip('/')
-
-
-if BOT_URL and BASE_URL:
-    @receiver(post_save, sender=Post, dispatch_uid='notify_bot')
-    def notify_bot(sender, instance, **kwargs):
-        if instance.is_op:
-            data = {
-                    'user': instance.user.username,
-                    'topic': instance.subject,
-                    'board': instance.board.title,
-                    'url': BASE_URL + instance.get_absolute_url(),
-                    }
-            resp = requests.post(BOT_URL+'/forum_post', json=data)
-
-            if not resp.ok:
-                raise Exception(resp.content)
+#BOT_URL = settings.THE_BOT.get('webhook', '').strip().rstrip('/')
+#BASE_URL = settings.THE_BOT.get('base_url', '').strip().rstrip('/')
+#
+#
+#if BOT_URL and BASE_URL:
+#    @receiver(post_save, sender=Post, dispatch_uid='notify_bot')
+#    def notify_bot(sender, instance, **kwargs):
+#        if instance.is_op:
+#            data = {
+#                    'user': instance.user.username,
+#                    'topic': instance.subject,
+#                    'board': instance.board.title,
+#                    'url': BASE_URL + instance.get_absolute_url(),
+#                    }
+#            resp = requests.post(BOT_URL+'/forum_post', json=data)
+#
+#            if not resp.ok:
+#                raise Exception(resp.content)
 
