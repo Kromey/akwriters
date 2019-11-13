b9f64e05120fb3f6fdf7578ff340283f3ec31f46
Travis Veazey <travisvz@gmail.com>
2019-11-13 10:03:51 -0900

    Make CharacterNoteAnswer inline in CharacterAdmin

--- tools/admin.py
+++ tools/admin.py
@@ -5,6 +5,12 @@ from .models import Character,CharacterNotes,CharacterNotesAnswer
 
 
 # Register your models here.
+class CharacterNotesAnswerInline(admin.TabularInline):
+    model = CharacterNotesAnswer
+    fields = ('question','answer',)
+    readonly_fields = ('question','answer',)
+
+
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     list_display = ('name','story',)
@@ -16,14 +22,11 @@ class CharacterAdmin(admin.ModelAdmin):
     readonly_fields = ('story',)
     select_related = ('story', 'story__owner')
 
+    inlines = (CharacterNotesAnswerInline,)
+
 
 @admin.register(CharacterNotes)
 class CharacterNotesAdmin(admin.ModelAdmin):
     list_display = ('question','is_active',)
 
-
-@admin.register(CharacterNotesAnswer)
-class CharacterNotesAnswerAdmin(admin.ModelAdmin):
-    list_display = ('character','question','answer',)
-    readonly_fields = ('character','question','answer',)
 
