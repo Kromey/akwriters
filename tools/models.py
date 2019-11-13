cac1b35a3240cf8d84c24a6acae7156314cfd003
Travis Veazey <travisvz@gmail.com>
2019-11-13 08:59:01 -0900

    New Story model, add existing characters to default Story

--- tools/models.py
+++ tools/models.py
@@ -4,8 +4,13 @@ from django.utils.text import Truncator
 
 
 # Create your models here.
+class Story(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=127)
+
 class Character(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     age = models.SmallIntegerField(null=True, blank=True)
     appearance = models.TextField(blank=True)
