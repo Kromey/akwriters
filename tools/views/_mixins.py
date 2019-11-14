5d79155587897c8bbf16c54c17b49e982e8c77db
Travis Veazey <travisvz@gmail.com>
2019-11-13 17:02:32 -0900

    Add Story field to Characters
    Allows authors to move characters from one story to another

--- tools/views/_mixins.py
+++ tools/views/_mixins.py
@@ -1,3 +1,6 @@
+from django.forms import ModelChoiceField
+
+
 from tools.models import Character,CharacterNotes
 
 
@@ -8,7 +11,7 @@ class OwnedItemsMixin:
 
 class CharacterFormMixin(object):
     model = Character
-    fields = ['owner','name','age','appearance',]
+    fields = ['owner','name','age','appearance','story',]
     context_object_name = 'character'
 
     def get_form_kwargs(self):
@@ -24,6 +27,18 @@ class CharacterFormMixin(object):
             pass
 
         return kwargs
+
+    def get_form_class(self):
+        form = super().get_form_class()
+
+        class StoryChoiceField(ModelChoiceField):
+            def label_from_instance(self, obj):
+                return obj.title
+
+        stories = self.request.user.story_set
+        form.base_fields['story'] = StoryChoiceField(queryset=stories)
+
+        return form
 
     def get_context_data(self):
         context = super().get_context_data()
